a3e604b04fa8c09d5fb66bf5577b4a9e3c571186
unknown <parsaaakarimi@gmail.com>
2024-07-22 14:06:56 +0330

    first commit

---
+++ src/components/App/Coins-data/Coins-data.js
@@ -0,0 +1,21 @@
+const btc = {
+       number : 1,
+       icon : "../../../images/btc-icon.svg",
+       name : 'Bitcoin BTC'
+
+    }
+
+const CoinsData = () => {
+    
+    return ( 
+
+      <>
+      
+      
+      
+      </>
+
+     );
+}
+ 
+export default CoinsData;
